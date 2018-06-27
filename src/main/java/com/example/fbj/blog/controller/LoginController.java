84a80e80942740a204f1eaa97c5fa7c7c333eb94
fubjie <fubjie@163.com>
2018-06-27 17:46:39 +0800

    version0

---
+++ src/main/java/com/example/fbj/blog/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.example.fbj.blog.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/test")
+    @ResponseBody
+    public String test(){
+        return "李健是个勺!";
+    }
+}
